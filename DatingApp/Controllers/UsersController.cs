be5829fe2c121f6e4b62b458869a3a57f06dce25
Kacper Bialy <kacper1400@o2.pl>
2023-05-07 23:40:25 +0200

    Building a walking skeleton - API

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+    public UsersController(DataContext dataContext)
+    {
+        _context = dataContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser([FromRoute] int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+
+        if (user is null)
+            return NotFound();
+
+        return user;
+    }
+}
